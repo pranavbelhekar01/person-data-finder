07ab284e5e0258a418427f3a0e174a5f3281c881
Pranav Belhekar <pranavbelhekar2002@gmail.com>
2025-01-24 23:17:37 +0530

    updated data

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class PersonInfoCrew:
                 PersonInfoTasks.information_retrieval(self.researcher),
                 PersonInfoTasks.format_response(self.researcher)
             ],
-            process=Process.hierarchical,
+            process=Process.sequential,
             manager_llm=config.llm_config,  # REQUIRED for hierarchical process
             verbose=True,
             memory=True
